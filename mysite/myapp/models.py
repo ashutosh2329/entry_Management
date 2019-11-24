d1a103a9f727943df27b4a5c4823f64393596e0f
ashutosh2329 <sonutiwari2329@gmail.com>
2019-11-25 01:19:48 +0530

    Third commit with dummydata and little queries display

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -9,6 +9,9 @@ class Host(models.Model):
     email = models.EmailField()
     phone = models.PositiveIntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class Visitor(models.Model):
     name = models.CharField(max_length=200,)
@@ -16,6 +19,17 @@ class Visitor(models.Model):
     phone = models.PositiveIntegerField()
     visiting_host = models.ForeignKey(Host,default=1,on_delete=models.SET_DEFAULT)
     checkin_time = models.DateTimeField("time arrived",default=datetime.now())
-    checkout_time = models.TimeField("time leaving")
+    checkout_time = models.TimeField("time leaving",blank=True, null=True)
+
+    def checkin(self):
+        self.checkin_time = datetime.now();
+        self.save();
+    
+    def checkout(self):
+        self.checkout_time = datetime.now()
+        self.save();
+    
+    def __str__(self):
+        return self.name
 
 
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Host
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world, Welcome to my App");
+    hosts = Host.objects.all()
+    context = {
+        'hosts':hosts
+    }
+
+    return render(request,"myapp_home.html",context);
